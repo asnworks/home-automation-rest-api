d0a9510b64e2fe5bd4439a64a6ba60f571895917
David Riesz <dbriesz@users.noreply.github.com>
2017-04-08 16:36:09 -0700

    Fix event handlers to restrict creating/modifying devices and controls. Delete DeviceEventHandler as it is not needed. CRUD operations are restricted for devices and controls based on whether the user is a room administrator.

--- src/main/java/com/teamtreehouse/control/ControlEventHandler.java
+++ src/main/java/com/teamtreehouse/control/ControlEventHandler.java
@@ -1,11 +1,9 @@
 package com.teamtreehouse.control;
 
-import com.teamtreehouse.room.Room;
 import com.teamtreehouse.user.User;
 import com.teamtreehouse.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.*;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -19,37 +17,10 @@ public class ControlEventHandler {
         this.users = users;
     }
 
-    @HandleBeforeCreate
-    public void addControlBasedOnLoggedInUser(Room room) {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = users.findByName(name);
-        if (!room.getAdministrators().contains(user)) {
-            throw new AccessDeniedException("Access Denied - user must be a room administrator in order to create controls");
-        }
-    }
-
-    @HandleBeforeSave
-    public void saveControlBasedOnLoggedInUser(Control control) {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = users.findByName(name);
-        if (!control.getDevice().getRoom().getAdministrators().contains(user)) {
-            throw new AccessDeniedException("Access Denied - user must be a room administrator in order to modify controls");
-        }
-    }
-
     @HandleAfterSave
     public void setLastModifiedAfterSave(Control control) {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = users.findByName(name);
         control.setLastModifiedBy(user);
-    }
-
-    @HandleBeforeDelete
-    public void deleteControlBasedOnLoggedInUser(Control control) {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = users.findByName(name);
-        if (!control.getDevice().getRoom().getAdministrators().contains(user)) {
-            throw new AccessDeniedException("Access Denied - user must be a room administrator in order to delete controls");
-        }
     }
 }
